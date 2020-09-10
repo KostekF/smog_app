56c56e266c519a7a7abcec96d9eb04fca580cf65
KostekF <filip.kostecki@student.put.poznan.pl>
2020-09-10 13:58:49 +0200

    Add function sending weather info messages

--- discord_msgs.py
+++ discord_msgs.py
@@ -2,6 +2,33 @@ import requests
 import json
 
 
+# TODO: create tests
+
+
+def send_weather_msg(webhookurl, measurements, *, standards=None, indexes=None, bot_name='weather'):
+    redacted_message = "Current weather: \n"
+    embed = {}
+    embed["title"] = "Weather"
+
+    for key, value in measurements.items():
+        redacted_message += f"{key:<12}: {value};\n"
+
+        # check if value exceeds limits given in standards list
+        if standards:
+            for standard in standards:
+                if key in standard['pollutant']:
+                    redacted_message += f"{key} {standard['name']} standard: {standard['limit']};"
+                    redacted_message += '\n\n'
+
+    if indexes:
+        for value in indexes:
+            redacted_message += f"{value['name']:<12} level of toxins in the air: {value['level']}\n"
+            embed['color'] = int(value['color'][1:], 16)
+
+    embed['description'] = redacted_message
+    send_msg(webhookurl, embed=embed, bot_name=bot_name)
+
+
 def send_msg(webhookurl, message="@here Your info is here!", *, embed=None, bot_name='temp'):
     data = {}
     data['content'] = message
